19c7fcb3ffc38fafb75220c2e5ed4c027a9a0f21
shinchan367 <98442984+shinchan367@users.noreply.github.com>
2024-01-23 01:17:21 +0530

    Update set mismatch.cpp

--- Array/set mismatch.cpp
+++ Array/set mismatch.cpp
@@ -1,1 +1,71 @@
 
+//Example 1:
+
+//Input: nums = [1,2,2,4]
+//Output: [2,3]
+
+//Example 2:
+
+//Input: nums = [1,1]
+//Output: [1,2]
+
+
+//Approach-1 (Smart Approach)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n = nums.size();
+        
+        int dup     = -1;
+        int missing = -1;
+        
+        for(int i = 0; i<n; i++) {
+            int num = abs(nums[i]);
+            
+            if(nums[num-1] < 0)
+                dup = num;
+            else
+                nums[num-1] *= (-1);
+        }
+        
+        for(int i = 0; i<n; i++) {
+            if(nums[i] > 0) {
+                missing = i+1;
+                break;
+            }
+        }
+        
+        return {dup, missing};
+    }
+};
+
+
+//Approach-2 (Basic Approach)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n = nums.size();
+        int miss = 0, dup = 0;
+        
+        map<int, int> mp;
+        for(int x:nums) {
+            mp[x]++;
+        }
+        
+        for(int i = 1; i<=n; i++) {
+            if(mp.count(i)) {
+                if(mp[i] == 2)
+                    dup = i;
+            } else
+                miss = i;
+        }
+        
+        return {dup, miss};
+    }
+};
+
+
