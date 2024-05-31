b976a77ee260f9d10107ae348107271d5a0de1a8
shinchan367 <98442984+shinchan367@users.noreply.github.com>
2024-06-01 00:26:21 +0530

    Create longest comon prefix.cpp

---
+++ string/longest comon prefix.cpp
@@ -0,0 +1,27 @@
+//Complexity
+//Time complexity: O(len(strs))
+//Space complexity: 
+//O(len(strs[0])
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+
+        string pref = strs[0];
+        int prefLen = pref.length();
+
+        for (int i = 1; i < strs.size(); i++) {
+            string s = strs[i];
+            while (prefLen > s.length() || pref != s.substr(0, prefLen)) {
+                prefLen--;
+                if (prefLen == 0) {
+                    return "";
+                }
+                pref = pref.substr(0, prefLen);
+            }
+        }
+
+        return pref;        
+    }
+};
