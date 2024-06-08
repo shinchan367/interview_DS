cbafd40e0170470da056ec8e3a10c0b5c7c94fa6
shinchan367 <98442984+shinchan367@users.noreply.github.com>
2024-06-08 12:16:35 +0530

    Create sort 0,1,2.cpp

---
+++ Array/sort 0,1,2.cpp
@@ -0,0 +1,18 @@
+ int n=nums.size();
+        int low=0,mid=0;
+        int high=n-1;
+        while(mid<=high)
+        {
+            if(nums[mid]==0){
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
+            }
+
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums[mid],nums[high]);
+                high--;
+            }
