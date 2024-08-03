923b5f205d66c66a40189d193bb0935076ce545f
Sanam manojna <98442984+shinchan367@users.noreply.github.com>
2024-08-03 22:57:57 +0530

    Create make-two-arrays-equal-by-reversing-subarrays.cpp

---
+++ hashmap/make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+       sort(target.begin(),target.end());
+       sort(arr.begin(),arr.end());
+       for(int i=0;i<target.size();i++){
+        if(target[i]!=arr[i]){
+            return false;
+        }
+       }
+       return true;
+
+    }
+};
+//*****************************************************************
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        // Frequency count for arr
+        unordered_map<int, int> arrFreq;
+        for (int num : arr) {
+            arrFreq[num]++;
+        }
+
+        for (int num : target) {
+            // If num does not appear in target, then arrays are not equal
+            if (arrFreq.find(num) == arrFreq.end()) return false;
+
+            // Decrement the frequency count for num and
+            // remove key if the count goes to 0
+            arrFreq[num]--;
+            if (arrFreq[num] == 0) {
+                arrFreq.erase(num);
+            }
+        }
+        return arrFreq.size() == 0;
+    }
+};
