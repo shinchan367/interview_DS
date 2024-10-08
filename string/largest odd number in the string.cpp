4bf03eeb7a916603b6c7613d35b723ed86f2830a
Sanam manojna <98442984+shinchan367@users.noreply.github.com>
2024-10-08 16:10:30 +0530

    Create largest odd number in the string.cpp

---
+++ string/largest odd number in the string.cpp
@@ -0,0 +1,13 @@
+ public:
+    string maxOdd(string s) {
+        // your code here
+       
+       for(int i=s.length()-1;i>=0;i--){
+           if(s[i]%2!=0){
+               string s1=s.substr(0,i+1);
+               return s1;
+           }
+       }
+       return "";
+    }
+};
